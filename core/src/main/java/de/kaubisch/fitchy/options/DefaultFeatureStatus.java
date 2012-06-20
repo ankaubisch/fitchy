43168e63e399cec512ddf773e41bb947c7e754e8
AndreasKaubisch <komtur@EasyNote-TS44HR.(none)>
2012-06-20 21:11:10 +0200

    add new FitchyOptions structure

---
+++ core/src/main/java/de/kaubisch/fitchy/options/DefaultFeatureStatus.java
@@ -0,0 +1,33 @@
+package de.kaubisch.fitchy.options;
+
+import de.kaubisch.fitchy.FeatureStatus;
+
+public enum DefaultFeatureStatus implements FeatureStatus {
+	ON("on", true),
+	OFF("off", false);
+
+	private boolean enabled, disabled;
+	private String systemName;
+	
+	private DefaultFeatureStatus(String systemName, Boolean status) {
+		this.systemName = systemName;
+		this.enabled = status != null && status;
+		this.disabled = status != null && !status;
+	}
+	
+	@Override
+	public String getSystemName() {
+		return systemName;
+	}
+
+	@Override
+	public boolean isEnabledStatus() {
+		return enabled;
+	}
+
+	@Override
+	public boolean isDisabledStatus() {
+		return disabled;
+	}
+
+}
